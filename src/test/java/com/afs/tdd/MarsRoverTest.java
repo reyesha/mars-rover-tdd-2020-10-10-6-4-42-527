ad8f94a8b9541fd128042a9a48e7823adab21fd6
Hannah Mae Reyes <hannah.mae.reyes@oocl.com>
2020-10-15 19:34:12 +0800

    [MarsRover] [Hannah] test: add test for command R with given location and heading S

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -59,4 +59,15 @@ class MarsRoverTest {
         assertEquals(0, marsRover.getLocationY());
         assertEquals("E", marsRover.getHeading());
     }
+
+    @Test
+    void should_x_0_y_0_heading_W_when_execute_given_x_0_y_0_heading_S_and_command_R() {
+        MarsRover marsRover = new MarsRover(0, 0, "S");
+
+        marsRover.executeCommands("R");
+
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("W", marsRover.getHeading());
+    }
 }
