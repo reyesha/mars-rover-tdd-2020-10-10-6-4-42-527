5abc7c988d7338ef87689543823ce13f07d386ab
Hannah Mae Reyes <hannah.mae.reyes@oocl.com>
2020-10-15 20:16:56 +0800

    [MarsRover] [Hannah] test: add command R with given location and heading W

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -125,4 +125,15 @@ class MarsRoverTest {
         assertEquals(0, marsRover.getLocationY());
         assertEquals("S", marsRover.getHeading());
     }
+
+    @Test
+    void should_x_0_y_0_heading_N_when_execute_given_x_0_y_0_heading_W_and_command_R() {
+        MarsRover marsRover = new MarsRover(0, 0, "W");
+
+        marsRover.executeCommands("R");
+
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("N", marsRover.getHeading());
+    }
 }
