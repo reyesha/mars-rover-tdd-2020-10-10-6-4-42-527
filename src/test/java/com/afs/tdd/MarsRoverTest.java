0a6d0c12ea8a49590168685277f232b98538d42a
Hannah Mae Reyes <hannah.mae.reyes@oocl.com>
2020-10-15 17:08:32 +0800

    [MarsRover] [Hannah] test: add test for command L with given location and heading N

---
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -0,0 +1,18 @@
+package com.afs.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MarsRoverTest {
+    @Test
+    void should_x_0_y_1_heading_N_when_execute_given_x_0_y_0_heading_N_and_command_M() {
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+
+        marsRover.executeCommands("M");
+
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(1, marsRover.getLocationY());
+        assertEquals("N", marsRover.getHeading());
+    }
+}
