9b5de892f6a2504c336368363af4012d6eb8fede
Hannah Mae Reyes <hannah.mae.reyes@oocl.com>
2020-10-15 19:16:40 +0800

    [MarsRover] [Hannah] feature: execute command R with given location and heading N

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -17,6 +17,14 @@ public class MarsRover {
             move();
         }else if (command.equals("L")){
             turnLeft();
+        }else if (command.equals("R")){
+            turnRight();
+        }
+    }
+
+    private void turnRight() {
+        if (heading.equals("N")) {
+            heading = "E";
         }
     }
 
